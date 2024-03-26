5feba80847251da06a5eb3d22da9d358003dc265
Benjamin Mayo <me@bzamayo.com>
2024-03-26 09:40:37 +0000

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -10,6 +10,7 @@ let package = Package(
 		// Products define the executables and libraries a package produces, and make them visible to other packages.
 		.library(
 			name: "XMTPiOS",
+			type: .dynamic,
 			targets: ["XMTPiOS"]
 		),
 		.library(
